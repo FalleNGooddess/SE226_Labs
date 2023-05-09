c26b77d900f66f3db70f41fc5ce18318f8a02073
Duygu <107173917+FalleNGooddess@users.noreply.github.com>
2023-05-09 11:53:18 +0300

    Add files via upload

--- LAB 7/lab7.py
+++ LAB 7/lab7.py
@@ -12,10 +12,9 @@ cv2.imshow('Red channel', red)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-new_blue_channel = np.zeros_like(blue)
-new_green_channel = np.zeros_like(green)
+new_green = np.zeros_like(green)
 
-modified_donut = cv2.merge([new_blue_channel, new_green_channel, red])
+modified_donut = cv2.merge([blue, new_green, red])
 
 cv2.imshow('Red and Blue Image', modified_donut)
 cv2.waitKey(0)
